3d233822e598cce2076e43c57fb55076a7819db4
Martin <martinconradpelayo@gmail.com>
2017-04-27 16:38:07 -0700

    no

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -11,4 +11,36 @@ describe('studios API', () => {
       assert.deepEqual(studios, []);
     });
   });
-});
+
+  let fakeStudio = {
+    name: 'Paramount',
+    address: {
+      city: 'LA', 
+      state: 'california'
+      country: 'USA',
+    }
+  }
+
+    function savedStudio(studio) {
+      return request.post('api/studios')
+        .send(studio)
+        .then(res => res.body);      
+    }
+  
+  it('roundtrips a studio', () => {
+    return savedStudio(fakeStudio)
+      .then(saved => {
+        assert.ok(saved._id, 'saved has id');
+        fakeStudio = saved;
+      })
+      .then(() => {
+        return request.get(`/api/studios ${fakeStudio._id}`);
+      })
+      .then(res => {
+        const studio = res.body;
+        assert.deepEqual(studio, [fakeStudio]);
+      });
+      it('')
+    });
+    
+  });
